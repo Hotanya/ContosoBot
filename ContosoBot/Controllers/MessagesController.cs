a86c15019c90f047323391193e5b7cbacdb1dbfa
Hotanya <hotanya.r@gmail.com>
2016-11-19 23:37:08 +1300

    Added a "welcome message" which greets user and asks for name. Stores user name to var

--- ContosoBot/Controllers/MessagesController.cs
+++ ContosoBot/Controllers/MessagesController.cs
@@ -7,6 +7,7 @@ using System.Web.Http;
 using System.Web.Http.Description;
 using Microsoft.Bot.Connector;
 using Newtonsoft.Json;
+using System.Text;
 
 namespace ContosoBot
 {
@@ -19,20 +20,59 @@ namespace ContosoBot
         /// </summary>
         public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
         {
+            // Global values
+            bool boolAskedForUserName = false;
+            string strUserName = "";
             if (activity.Type == ActivityTypes.Message)
             {
+                // Get any saved values
+                StateClient sc = activity.GetStateClient();
+                BotData userData = sc.BotState.GetPrivateConversationData(
+                    activity.ChannelId, activity.Conversation.Id, activity.From.Id);
+                boolAskedForUserName = userData.GetProperty<bool>("AskedForUserName");
+                strUserName = userData.GetProperty<string>("UserName") ?? "";
+                // Create text for a reply message   
+                StringBuilder strReplyMessage = new StringBuilder();
+                if (boolAskedForUserName == false) // Never asked for name
+                {
+                    strReplyMessage.Append($"Hello, I am the Contoso Bot!");
+                    strReplyMessage.Append($"\n");
+                    strReplyMessage.Append($"You can say anything");
+                    strReplyMessage.Append($"\n");
+                    strReplyMessage.Append($"to me and I will repeat it back");
+                    strReplyMessage.Append($"\n\n");
+                    strReplyMessage.Append($"What is your name?");
+                    // Set BotUserData
+                    userData.SetProperty<bool>("AskedForUserName", true);
+                }
+                else // Have asked for name
+                {
+                    if (strUserName == "") // Name was never provided
+                    {
+                        // If we have asked for a username but it has not been set
+                        // the current response is the user name
+                        strReplyMessage.Append($"Hello {activity.Text}, how may I help you?");
+                        // Set BotUserData
+                        userData.SetProperty<string>("UserName", activity.Text);
+                    }
+                    else // Name was provided
+                    {
+                        strReplyMessage.Append($"{strUserName}, You said: {activity.Text}");
+                    }
+                }
+                // Save BotUserData
+                sc.BotState.SetPrivateConversationData(
+                    activity.ChannelId, activity.Conversation.Id, activity.From.Id, userData);
+                // Create a reply message
                 ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
-                // calculate something for us to return
-                int length = (activity.Text ?? string.Empty).Length;
-
-                // return our reply to the user
-                Activity reply = activity.CreateReply($"You sent {activity.Text} which was {length} characters");
-                await connector.Conversations.ReplyToActivityAsync(reply);
+                Activity replyMessage = activity.CreateReply(strReplyMessage.ToString());
+                await connector.Conversations.ReplyToActivityAsync(replyMessage);
             }
             else
             {
                 HandleSystemMessage(activity);
             }
+            // Return response
             var response = Request.CreateResponse(HttpStatusCode.OK);
             return response;
         }
@@ -41,8 +81,20 @@ namespace ContosoBot
         {
             if (message.Type == ActivityTypes.DeleteUserData)
             {
-                // Implement user deletion here
-                // If we handle user deletion, return a real message
+                // Get BotUserData
+                StateClient sc = message.GetStateClient();
+                BotData userData = sc.BotState.GetPrivateConversationData(
+                    message.ChannelId, message.Conversation.Id, message.From.Id);
+                // Set BotUserData
+                userData.SetProperty<string>("UserName", "");
+                userData.SetProperty<bool>("AskedForUserName", false);
+                // Save BotUserData
+                sc.BotState.SetPrivateConversationData(
+                    message.ChannelId, message.Conversation.Id, message.From.Id, userData);
+                // Create a reply message
+                ConnectorClient connector = new ConnectorClient(new Uri(message.ServiceUrl));
+                Activity replyMessage = message.CreateReply("Personal data has been deleted.");
+                return replyMessage;
             }
             else if (message.Type == ActivityTypes.ConversationUpdate)
             {
